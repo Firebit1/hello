8c98d59aaa3bedc7c91a61ec37e44a94b7b9037f
Preston Pinkelman <ppinkelman2@huskers.unl.edu>
2022-08-25 18:34:53 +0000

    added my major to the output

--- hello.c
+++ hello.c
@@ -1,1 +1,14 @@
 //  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
+
+//  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
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Preston Pinkelman, the Destroyer of Worlds\n");
+  printf("Major: Computer Science\nStatus: Yes\n");
+
+  return 0;
+}
