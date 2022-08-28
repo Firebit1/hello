18d4db83396ec0afe6e4cad26ad685c7be32c298
barrettrodell <111595987+barrettrodell@users.noreply.github.com>
2022-08-28 20:58:03 +0000

    added partners name/major

--- hello.c
+++ hello.c
@@ -6,6 +6,8 @@ int main(int argc, char **argv) {
 
   printf("Preston Pinkelman, the Destroyer of Worlds\n");
   printf("Major: Computer Science\nStatus: Yes\n");
+  printf("Barrett Rodell\n");
+  printf("Computer Science");
 
   return 0;
 }
