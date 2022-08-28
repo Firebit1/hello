2f195112c832ac0c7a6fe25447e8497efb86eee4
Preston Pinkelman <ppinkelman2@huskers.unl.edu>
2022-08-28 21:03:44 +0000

    Added a block header comment

--- hello.c
+++ hello.c
@@ -1,3 +1,10 @@
+/**
+ *  Name: Preston Pinkelman
+ *        ppinkelman2@huskers.unl.edu
+ *  Date: 28 Aug 2022
+ *
+ *  This program prints my name & major, as well as my partner's.
+ */
 
 #include <stdlib.h>
 #include <stdio.h>
